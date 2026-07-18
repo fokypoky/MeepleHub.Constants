45fba84a1ea161d7dcff017a48880167f92c7b74
Anton Petukhov <petuhova805@gmail.com>
2026-07-19 02:34:06 +0300

    init

---
+++ MeepleHub.Constants/Errors.cs
@@ -0,0 +1,13 @@
+﻿namespace MeepleHub.Constants
+{
+    public static class Errors
+    {
+        public const string USER_ALREADY_EXISTS = "MH_1";
+
+        public const string USER_WITH_CREDENTIALS_NOT_EXISTS = "MH_2";
+
+        public const string USER_WITH_ID_NOT_EXISTS = "MH_3";
+
+        public const string INTERNAL_EXCEPTION = "MH_500";
+    }
+}
