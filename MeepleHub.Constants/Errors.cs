29d69edcb6d369ce5db47681cd3ad0781555a2d3
Anton Petukhov <petuhova805@gmail.com>
2026-07-19 03:07:06 +0300

    feature: Добавил коды параметров

--- MeepleHub.Constants/Errors.cs
+++ MeepleHub.Constants/Errors.cs
@@ -3,11 +3,8 @@
     public static class Errors
     {
         public const string USER_ALREADY_EXISTS = "MH_1";
-
         public const string USER_WITH_CREDENTIALS_NOT_EXISTS = "MH_2";
-
         public const string USER_WITH_ID_NOT_EXISTS = "MH_3";
-
         public const string INTERNAL_EXCEPTION = "MH_500";
     }
 }
---
+++ MeepleHub.Constants/ParameterCodes.cs
@@ -0,0 +1,8 @@
+﻿namespace MeepleHub.Constants
+{
+    public static class ParameterCodes
+    {
+        public const string USER_ID = "user.id";
+        public const string USER_LOGIN = "user.login";
+    }
+}
